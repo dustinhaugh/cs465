4a236d98bf12b22b02a3c2164394092d9fc400db
dustinhaugh <dustinhaugh@hotmail.com>
2023-05-21 23:17:05 -0600

    Fix HBS

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const hbs = require('hbs');
 
 // set up routers below
 var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var usersRouter = require('./app_server/routes/users');
+
 // DUSTIN HAUGH: I will inster app-server paths below
 
 
@@ -20,10 +21,9 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (NPM, 2021, p. 1)
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
-
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));8
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
