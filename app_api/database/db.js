b144027b7b3bd3628ca65d2638f1820cc2b67015
dustinhaugh <dustinhaugh@hotmail.com>
2023-06-14 17:27:10 -0600

    Edit app_admin Trip model

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,70 +1,73 @@
-// Using Node.js `require()`
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const host = process.env.DB_HOST || "127.0.0.1";
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require("readline");
 
-const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = `mongodb://127.0.0.1/travlr`;
-const readLine = require('readline');
-
-// avoid 'current Server Discover and Monitorying engine is depreciated'
-mongoose.set('useUnifiedTopology', true);
+// avoid current server discovery and monitoring engine is depreciated
+mongoose.set("useUnifiedTopology", true);
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, 
-        { useNewUrlParser: true, 
-            useCreateIndex: true 
-        }), 1000);
-  }
+  setTimeout(
+    () =>
+      mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }),
+    1000
+  );
+};
 
-mongoose.connection.on('connected', () => {
-  console.log('connected');
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${dbURI}`);
 });
 
-mongoose.connection.on('error', err => {
-  console.log('error: ' + err);
-  return connect();
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection error: `, err);
 });
 
-mongoose.connection.on('disconnected', () => {
-  console.log('disconnected');
+mongoose.connection.on("disconnected", () => {
+  console.log(`Mongoose disconnected`);
 });
 
-if (process.platform === 'win32') {
+if (process.platform == "win32") {
   const rl = readLine.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
-  rl.on ('SIGINT', () => {
+  rl.on("SIGINT", () => {
     process.emit("SIGINT");
   });
 }
 
 const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close( () => {
+  mongoose.connection.close(() => {
     console.log(`Mongoose disconnected through ${msg}`);
     callback();
   });
 };
 
-// For nondemon restarts
-process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart', () => {
-    process.kill(process.pid, 'SIGUSR2');
+// nodemon restarts
+process.once("SIGUSR2", () => {
+  gracefulShutdown("nodemon restart", () => {
+    process.kill(process.pid, "SIGUSR2");
   });
 });
-// For app termination
-process.on('SIGINT', () => {
-  gracefulShutdown('app termination', () => {
+
+// app termination
+process.on("SIGINT", () => {
+  gracefulShutdown("app termination", () => {
     process.exit(0);
   });
 });
-// For Heroku app termination
-process.on('SIGTERM', () => {
-  gracefulShutdown('Heroku app shutdown', () => {
+
+// Herokue app termination
+process.on("SIGTERM", () => {
+  gracefulShutdown("Heroku app shutdown", () => {
     process.exit(0);
   });
 });
 
 connect();
 
-// bring in Mongoose schema
-require('./models/travlr');
+// bring in schema
+require("./models/travlr");
