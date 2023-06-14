a045153e947af90b4425a9bad4e63ca930836765
dustinhaugh <dustinhaugh@hotmail.com>
2023-06-14 17:45:00 -0600

    Module 6 Tested Good

--- app.js
+++ app.js
@@ -48,8 +48,6 @@ app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
-
-
 });
 
 
