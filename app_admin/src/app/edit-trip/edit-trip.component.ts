f402802949c1beaa1d9ab892402816d02b1374d7
dustinhaugh <dustinhaugh@hotmail.com>
2023-06-18 16:03:01 -0600

    screwing things up

--- app_admin/src/app/edit-trip/edit-trip.component.ts
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core'; 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { TripDataService } from '../services/trip-data.service';
 import { AuthenticationService } from '../services/authentication.service';
 
@@ -10,72 +10,66 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./edit-trip.component.css']
 })
 export class EditTripComponent implements OnInit {
-  
-  editForm!: FormGroup; 
-  submitted = false; 
-  
-  constructor( 
-    private formBuilder: FormBuilder, 
-    private router: Router, 
+
+  editForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
     private tripService: TripDataService,
     private authenticationService: AuthenticationService
+
   ) { }
-  
+
   ngOnInit() {
-    // retrieve stashed tripId 
-    let tripCode = localStorage.getItem("tripCode"); 
-    if (!tripCode) {
-      alert ("Something wrong, counldn't find where I stashed tripCode!"); 
-      this.router.navigate(['']); 
+    //retrive stashed tripId
+    let tripCode = localStorage.getItem("tripCode");
+    if(!tripCode) {
+      alert ("Something is wrong, couldn't find stashed tripCode!");
+      this.router.navigate(['']);
       return;
-    } 
-    
-    console.log('EditTripComponent#onInit found tripCode ' + tripCode); 
-    
-    //initialize form 
-    this.editForm = this.formBuilder.group({
-      _id: [], 
-      code: [tripCode, Validators.required], 
-      name: ['', Validators.required], 
-      length: ['', Validators.required], 
-      start: ['', Validators.required], 
-      resort: ['', Validators.required], 
-      perPerson: ['', Validators.required], 
-      image: ['', Validators.required], 
-      description: ['', Validators.required],
+    }
+
+    console.log("EditTripComponent#onInit found tripCode " + tripCode);
+
+    //initialize form
+    this.editForm = this.formBuilder.group ({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
     })
-    
-    console.log('EditTripComponent#onInit calling TripDataService#getTrip(\'' + tripCode + '\')');
 
     this.tripService.getTrip(tripCode)
       .then(data => {
         console.log(data);
-        // Don't use editForm.setValue() as it will throw
-        this.editForm.patchValue(data);
+        //Don't use editFrom.setValue() this will throw console error
+        this.editForm.patchValue(data[0]);
       })
   }
-  
+
   onSubmit() {
     this.submitted = true;
-
     if (this.editForm.valid) {
+      
       const token = this.authenticationService.getToken();
+      // create authorization header with bearer token attribute (Savani, 2023, p. 1)
       const headers = {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}` 
       };
 
-
-      this.tripService.updateTrip(this.editForm.value)
-        .then((data: any) => {
-          console.log(data);
-          this.router.navigate(['']);
-        });
+      // call edit trip from the trip service object passing the values of edit form and the authorization bearer token (Savani, 2023, p. 1);(SNHU, 2023, p. 1)
+      this.tripService.updateTrip(this.editForm.value, headers)
+      .then(() => {
+        this.router.navigate(['/list-trips']);
+      });
+      
     }
   }
-
-  // get the form short name to access the form fields 
-  get f() { return this.editForm.controls; }
-
 }
-
-
